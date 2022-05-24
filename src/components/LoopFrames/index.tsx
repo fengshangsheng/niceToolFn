6a5b4775a7df4774c3e7a1f8adb4413c6a489d2e
fengshangsheng <1820959467@qq.com>
2022-05-24 17:11:16 +0800

    帧动画组件-优化

--- src/components/LoopFrames/index.tsx
+++ src/components/LoopFrames/index.tsx
@@ -1,18 +1,5 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { setRequestAnimationFrame } from '../../helpers/requestAnimationFrame';
-import useFirstState from '../../hook/useFirstState';
-
-
-const _styleUl: { [key: string]: any } = {
-  display: 'inline-block',
-  position: 'relative'
-}
-const _styleLi: { [key: string]: any } = {
-  position: 'absolute',
-  userSelect: 'none',
-  left: 0,
-  top: 0
-}
 
 type IProps = {
   frames: string[] // 帧图片
@@ -22,23 +9,15 @@ type IProps = {
 }
 
 const LoopFrames = (props: IProps) => {
-  const isFirst = useFirstState();
   const _refClearFn = useRef<undefined | (() => void)>();
-  // 已加载的帧数量
-  const [loadFrame, triggerLoadFrame] = useState<string[]>([]);
+  const [frames] = useState<string[]>(props.frames);
+  const [loadEnd, triggerLoadEnd] = useState<boolean>(false);
   // 当前显示的关键帧
-  const [frame, updateFrame] = useState(0);
+  const [frameIdx, triggerFrameIdx] = useState(0);
 
-  const updateLoaded = (frame: string) => {
-    const flag = loadFrame.includes(frame)
-    if (!flag) {
-      triggerLoadFrame([...loadFrame, frame]);
-    }
-  }
-
-  const hasLoadEnd = useMemo<boolean>(() => {
-    return loadFrame.length === props.frames.length
-  }, [loadFrame.length]);
+  const windowOnLoad = useCallback(() => {
+    triggerLoadEnd(true);
+  }, []);
 
   const play = () => {
     stop();
@@ -49,52 +28,48 @@ const LoopFrames = (props: IProps) => {
   }
   const changeNextFrames = () => {
     _refClearFn.current = setRequestAnimationFrame(() => {
-      let newFrame = frame + 1;
-      newFrame = newFrame > props.frames.length - 1 ? 0 : newFrame;
-      updateFrame(newFrame);
-    }, props.pace || 60);
+      let newFrame = frameIdx + 1;
+      newFrame = newFrame > frames.length - 1 ? 0 : newFrame;
+      triggerFrameIdx(newFrame);
+    }, props.pace);
   }
 
   useEffect(() => {
-    if (hasLoadEnd && [undefined, false].includes(props.pause)) {
-      play()
-    }
-    return () => stop();
-  }, [hasLoadEnd]);
-  useEffect(() => {
-    if (isFirst) {
-      return;
-    }
-    // 暂停状态，激活帧数非首张
-    if (props.pause === true && frame !== 0) {
-      play();
-      return;
-    }
-    if (props.pause === false) {
-      play();
-      return;
-    }
-  }, [props.pause])
-  useEffect(() => {
-    if (isFirst) {
-      return;
-    }
-    // 暂停状态，且激活帧数为第首张
-    if (props.pause === true && frame === 0) {
-      return;
-    }
-    changeNextFrames();
-  }, [frame]);
+    window.addEventListener('load', windowOnLoad)
+    return () => window.removeEventListener('load', windowOnLoad);
+  }, []);
 
-  return <ul className={props.className || ''} style={_styleUl}>
-    {props.frames.map((item, index) => (
-      <li key={index} style={({ ..._styleLi, opacity: frame === index ? 1 : 0 })}>
-        <img src={item} alt=""
-             onLoad={() => updateLoaded(item)}
-             onError={() => {updateLoaded(item)}}
-        />
-      </li>
-    ))}
-  </ul>
+  useEffect(() => {
+    if (!loadEnd) {
+      return;
+    }
+    if (props.pause === undefined || !props.pause) {
+      play();
+      return;
+    }
+  }, [loadEnd]);
+  useEffect(() => {
+    if (!loadEnd) {
+      return;
+    }
+    if (props.pause && frameIdx === 0) {
+      return;
+    }
+    play();
+  }, [frameIdx]);
+  useEffect(() => {
+    if (!loadEnd) {
+      return;
+    }
+    if (props.pause) {
+      frameIdx === 0 && stop()
+      frameIdx !== 0 && play();
+    }
+    if (!props.pause) {
+      play();
+    }
+  }, [props.pause]);
+
+  return <img src={frames[frameIdx]} className={props.className} alt=''/>
 }
 export default LoopFrames;
